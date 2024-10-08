48a7b35093cb772351389c0cd80c6e6814836e34
Sharon Nrangi <sharonnarangi@gmail.com>
2024-10-08 20:47:04 +0300

    Javascript

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const API_LINK = "https://api.adviceslip.com";
+const adviceNumberSpan = document.querySelector("Advice-Generator-Advice-Number")
+const adviceQuoteSlot = document.querySelector("Advice-Generator-Quote")
+const fetchBtn = document.querySelector(" button.Advice-Generator-Btn")
 
 
-const fetchNewAdvice = () => {
-     const response = fetch ( API_LINK);
-     console.log(response)
+
+const fetchNewAdvice =  async () => {
+     const response = await fetch ( API_LINK);
+     const advice =  await response.json()
+     return advice
+};
+const renderAdvice = (adviceObj) =>{
+    const {id,advice} =adviceObj;
+    adviceNumberSpan.textContent = `ADVICE #${id}`;
+    adviceQuoteSlot.textContent = advice;
+}
+
+
+const generateNewAdvice = async () =>{
+
+    const data = await fetchNewAdvice()
+    const advice = data.slip;
+
+    renderAdvice(advice)
 };
 
-
-fetchNewAdvice()
+window.addEventListener('DOMContentLoaded',()=>{
+    fetchBtn.addEventListener("click",generateNewAdvice)
+})
