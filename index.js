3afaf7b3792a14e7667044c5bf49dadd9f6dc784
Sharon Nrangi <sharonnarangi@gmail.com>
2024-10-10 20:11:51 +0300

    Javacript

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const adviceQuoteSlot = document.querySelector(".Advice-Generator-Quote");
 const fetchBtn = document.querySelector(" button.Advice-Generator-Btn")
 
 
-
-const fetchNewAdvice =  async () => {
-     const response = await fetch ( API_LINK);
-     const advice =  await response.json()
-     return advice
+const fetchNewAdvice = async () => {
+    try {
+        const response = await fetch(API_LINK);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const advice = await response.json();
+        return advice;
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }      
 };
+
+
 const renderAdvice = (adviceObj) =>{
     const {id,advice} =adviceObj;
     adviceNumberSpan.textContent = `ADVICE #${id}`;
